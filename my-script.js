87259e1169e91b833e625b6af64aac29611c35a1
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-28 14:55:51 +0100

    Fill in the palette on the window's 'load' with random colours

---
+++ my-script.js
@@ -0,0 +1,29 @@
+// Create an array with palette variations
+// I want to add this functional later
+const colourValues = [
+    '#EDEEC9', '#DDE7C7', '#BFD8BD', '#98C9A3', '#77BFA3'
+];
+
+// Get the palette container element
+const memoryGameContainer = document.querySelector('.container');
+// Get the array of tiles
+const tiles = document.querySelectorAll('.tile')
+
+//
+window.addEventListener('load', () => {
+    // Set random background colors for each tile
+    tiles.forEach(tile => {
+        tile.style.backgroundColor = getRandomColour();
+    });
+});
+
+// Function to generate a random colour
+function getRandomColour() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
