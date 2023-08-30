2bd26fb3fa53106216653fcf2d35ed0a773ce7fa
Zukhra <zuhra.galieva.2014@gmail.com>
2023-08-30 12:46:39 +0100

    Clean the code

--- my-script.js
+++ my-script.js
@@ -4,34 +4,10 @@ const colourValues = [
     '#EDEEC9', '#DDE7C7', '#BFD8BD', '#98C9A3', '#77BFA3'
 ];
 
-// Get the palette container element
-const memoryGameContainer = document.querySelector('.container');
-// Get the array of tiles
-const tiles = document.querySelectorAll('.tile')
+function setTileBackgroundColor(tile, color) {
+    tile.style.backgroundColor = color;
+}
 
-window.addEventListener('load', () => {
-    // Set random background colors for each tile
-    tiles.forEach(tile => {
-        const randomColor = getRandomColour()
-        tile.style.backgroundColor = randomColor;
-
-        // Set the color name at the bottom of the tile
-        const colorNameElement = tile.querySelector('.color-name');
-        colorNameElement.textContent = randomColor;
-
-        const colorPicker = tile.querySelector('input[type="color"]');
-        colorNameElement.addEventListener('click', function() {
-            colorPicker.click(); // Automatically open color picker
-
-            colorPicker.addEventListener('input', function() {
-                tile.style.backgroundColor = colorPicker.value;
-                colorNameElement.textContent = colorPicker.value;
-            }); // Set tile background color
-        });
-    });
-});
-
-// Function to generate a random colour
 function getRandomColour() {
     const letters = '0123456789ABCDEF';
     let color = '#';
@@ -40,4 +16,45 @@ function getRandomColour() {
     }
     return color;
 }
+
+function setColorName(element, color) {
+    element.textContent = color;
+}
+
+function openColorPickerAndSetBackground(tile, colorNameElement) {
+    const colorPicker = tile.querySelector('input[type="color"]');
+
+    colorNameElement.addEventListener('click', function() {
+        colorPicker.click(); // Automatically open color picker
+    });
+
+    // Set tile background color
+    colorPicker.addEventListener('input', function() {
+        const selectedColor = colorPicker.value;
+        setTileBackgroundColor(tile, selectedColor);
+        setColorName(colorNameElement, selectedColor);
+    });
+}
+
+function initializeTiles() {
+    const tiles = document.querySelectorAll('.tile');
+
+    tiles.forEach(tile => {
+        // Set random background colors for each tile
+        const randomColor = getRandomColour();
+        setTileBackgroundColor(tile, randomColor);
+
+        // Set the color name at the bottom of the tile
+        const colorNameElement = tile.querySelector('.color-name');
+        setColorName(colorNameElement, randomColor);
+
+        openColorPickerAndSetBackground(tile, colorNameElement);
+    });
+}
+
+// Main code
+document.addEventListener('DOMContentLoaded', function() {
+    initializeTiles();
+});
+
 
