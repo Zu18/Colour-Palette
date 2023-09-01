a7fc79ef811448d3e44723f2ac82dfe60975a427
Zukhra <zuhra.galieva.2014@gmail.com>
2023-09-01 18:04:20 +0100

    Add functionality that allows swapping tiles

--- my-script.js
+++ my-script.js
@@ -52,6 +52,18 @@ function initializeTiles() {
         
         // Add the buttons that allows to remove tiles
         removeTile();
+
+        let items = document.querySelectorAll('.container .tile');
+        items.forEach(function (item) {
+            item.setAttribute('draggable', true);
+            item.addEventListener('dragstart', handleDragStart);
+            item.addEventListener('dragover', handleDragOver);
+            item.addEventListener('dragenter', handleDragEnter);
+            item.addEventListener('dragleave', handleDragLeave);
+            item.addEventListener('dragend', handleDragEnd);
+            item.addEventListener('drop', handleDrop);
+        });
+
     });
 }
 
@@ -73,4 +85,58 @@ function removeTile() {
             }
         });
     });
+}
+
+// Swap elements (tiles)
+function handleDragStart(e) {
+    this.style.opacity = '0.4';
+
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', this.innerHTML);
+}
+
+function handleDragEnd(e) {
+    this.style.opacity = '1';
+
+    const items = document.querySelectorAll('.tile')
+    items.forEach(function (item) {
+        item.classList.remove('over');
+    });
+}
+
+function handleDragOver(e) {
+    e.preventDefault();
+    return false;
+}
+
+function handleDragEnter(e) {
+    this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+    this.classList.remove('over');
+}
+
+function handleDrop(e) {
+    e.stopPropagation();
+
+    if (dragSrcEl !== this) {
+        // Swap the background colors
+        const dragSrcColor = dragSrcEl.style.backgroundColor;
+        const dropTargetColor = this.style.backgroundColor;
+
+        dragSrcEl.style.backgroundColor = dropTargetColor;
+        this.style.backgroundColor = dragSrcColor;
+
+        // Swap the color names
+        const dragSrcName = dragSrcEl.querySelector('.color-name').textContent;
+        const dropTargetName = this.querySelector('.color-name').textContent;
+
+        dragSrcEl.querySelector('.color-name').textContent = dropTargetName;
+        this.querySelector('.color-name').textContent = dragSrcName;
+    }
+
+    return false;
 }
